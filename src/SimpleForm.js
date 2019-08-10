29d6de1a3b5936216621d8bb9c446502f8108985
fehmikaya <fehmikaya@gmail.com>
2019-08-10 15:56:55 +0300

    local host heroku config final

--- src/SimpleForm.js
+++ src/SimpleForm.js
@@ -27,7 +27,6 @@ class SimpleForm extends React.Component {
   };
 
   getDataFromDb = () => {
-    console.log(process.env.NODE_ENV+","+config.HOST);
     fetch(config.HOST+"/api/getData")
       .then(data => data.json())
       .then(res => this.setState({ data: res.data }));
--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
-var env = process.env.NODE_ENV || 'development';
 var HOST = "https://pulseapidev.herokuapp.com";
 
-if (env === 'development') {
-    HOST = "";
+if (process.env.NODE_ENV === 'development') {
+    HOST = " http://localhost:3001";
 }
 
 module.exports = {HOST}; 
